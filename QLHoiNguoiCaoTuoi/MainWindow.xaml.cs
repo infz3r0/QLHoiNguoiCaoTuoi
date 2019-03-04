502569bdb84373d186843554bace82b94fab79b6
ifzr <infz3r0@gmail.com>
2019-03-04 19:19:19 +0700

    Basic UI on main window

--- QLHoiNguoiCaoTuoi/MainWindow.xaml.cs
+++ QLHoiNguoiCaoTuoi/MainWindow.xaml.cs
@@ -20,9 +20,36 @@ namespace QLHoiNguoiCaoTuoi
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool spnAccount_isMouseDown;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void spnAccount_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            spnAccount_isMouseDown = true;
+        }
+
+        private void spnAccount_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (spnAccount_isMouseDown)
+            {
+                spnAccount.ContextMenu.IsEnabled = true;
+                spnAccount.ContextMenu.IsOpen = true;
+                spnAccount.ContextMenu.PlacementTarget = spnAccount;
+                spnAccount.ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Relative;
+                spnAccount.ContextMenu.VerticalOffset = spnAccount.ActualHeight;
+                spnAccount.ContextMenu.HorizontalOffset = spnAccount.ActualWidth - spnAccount.ContextMenu.ActualWidth;
+                
+                spnAccount_isMouseDown = false;
+            }
+        }
+
+
+
+
+        //end
     }
 }
